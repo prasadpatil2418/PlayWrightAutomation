b53db3eef3b7f55425c1e1d2e944e8d399fedfbb
Prasad.Patil <prasadpatil481986@gmail.com>
2024-11-08 15:05:04 +0530

    first commit

---
+++ tests/Locators.spec.js
@@ -0,0 +1,46 @@
+const {test,expect} = require('@playwright/test')
+//import{test,expect} from '@playwright/test'
+
+test('Locators',async({page})=>{
+
+    await page.goto("https://www.demoblaze.com/")
+
+    //click on login button -property
+    //await page.locator('#login2').click() //1st method
+    await page.click('#login2') // 2nd method
+
+    //provide username
+    //await page.locator('#loginusername').fill("pavanol") //1st method
+   // await page.type('#loginusername','pavanol') // 2nd method
+    await page.fill('#loginusername','pavanol')  // 3rd method
+
+  //provide password
+  await page.fill("input[id='loginpassword']",'test@123')
+
+  //click on login button
+  await page.click("button[onclick='logIn()']")
+
+  //verify logout link presence
+
+  const logoutlink = await page.locator('#logout2')
+  await expect(logoutlink).toBeVisible();
+
+  await page.click('#logout2')
+  
+  await page.close()
+})
+
+test('MultipleElements',async ({page})=>{
+
+    await page.goto("https://www.demoblaze.com/")
+
+     const links= await page.$$('a') // locate multiple element 
+
+     for(const link of links){
+
+       const linktext = await link.textContent()
+       console.log(linktext)
+
+     }
+
+})
